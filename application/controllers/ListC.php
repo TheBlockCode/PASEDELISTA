caf8d7d306fac6c6973a2250ce4e233f52355e62
David Segovia Ley <starburstx64@gmail.com>
2019-04-01 23:40:30 -0600

    Intento de CRUD

--- application/controllers/ListC.php
+++ application/controllers/ListC.php
@@ -3,8 +3,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class ListC extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->database();
+    }
+
     public function index()
     {
-        $this->load->view('list');
+        //$this->load->view('list');
+    }
+
+    public function selectAll()
+    {
+        $crud = new grocery_CRUD();
+        $crud->set_table('Alumnos');
+        $output->crud->render();
     }
 }
